9fa17fb2249b775419cd9a9a5d87903734a623ca
C Anil Kumar <sravananil75@gmail.com>
2025-09-27 15:17:40 +0530

    Create api.ts

---
+++ api.ts
@@ -0,0 +1,21 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: "http://localhost:5000",
+  withCredentials: true,
+});
+
+api.interceptors.request.use(
+  (config) => {
+    const tokens = JSON.parse(localStorage.getItem('shortloom_auth') || '{}');
+    const token = tokens?.access_token;
+    if (token) {
+      config.headers = config.headers || {};
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
+export default api;
